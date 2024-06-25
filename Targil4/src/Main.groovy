1457d1caa5061a1c9f9d45334ca854ad069f31b2
Ariel Moreshet <arialaamm@gmail.com>
2024-06-25 22:11:59 +0300

    Targil4

--- Targil4/src/Main.groovy
+++ Targil4/src/Main.groovy
@@ -1,3 +1,20 @@
-static void main(String[] args) {
-  println "Hello world!"
+import javax.swing.JFileChooser
+public class FileUtils {
+  def String chooseFile(){
+    def initialDirectory = new File("C:\\Users\\arial\\groovy\\nand2tetris\\projects")
+    def fileChooser = new JFileChooser(initialDirectory)
+    fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY)
+    int returnValue = fileChooser.showOpenDialog(null)
+
+    if (returnValue == JFileChooser.APPROVE_OPTION) {
+      def directory = fileChooser.getSelectedFile()
+      return directory.absolutePath
+    }
+  }
 }
+
+FileUtils fileUtils = new FileUtils()
+def path = fileUtils.chooseFile()
+def folder = new File(path)
+Tokenizing t = new Tokenizing()
+def outputFile = t.tokenizing(folder)
